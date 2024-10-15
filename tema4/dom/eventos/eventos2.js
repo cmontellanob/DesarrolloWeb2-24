213547b28518d73a6e1b4bb34c656658a7247672
cmontellano <carlosmontellano@gmail.com>
2024-10-15 07:22:36 -0400

    14-10-24

---
+++ tema4/dom/eventos/eventos2.js
@@ -0,0 +1,12 @@
+function cargar(){
+    alert("La página se ha cargado completamente");
+}
+
+function pinchar(){
+    alert("Gracias por pinchar");
+}
+
+function mover(){
+    alert("Acabas de pasar el ratón por encima");
+}
+
